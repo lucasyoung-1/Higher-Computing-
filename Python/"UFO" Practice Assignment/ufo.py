72f544670d41564552ecdc5facc5b6511aca436e
Lucas Young <lucasyoung66@yahoo.com>
2025-02-03 21:32:33 +0000

    Update ufo.py

--- Python/"UFO" Practice Assignment/ufo.py
+++ Python/"UFO" Practice Assignment/ufo.py
@@ -18,17 +18,6 @@ def importFile():
             description.append(row[6])
     return thisDate, country, location, shape, description
 # -------------------------------------------------- DO NOT ALTER -----
-
-# # CountSightings()
-# Count the number of sightings for a specified country.
-# IN: country[], specifiedCountry
-# OUT: numSightings
-
-# DisplaySightings()
-# Display the number of sightings for a specific country.
-# IN: specifiedCountry, numSightings
-# OUT:
-
 def CountSightings(country, specifiedCountry):
     numSightings = 0
     for index in range(0,len(country)):
@@ -36,18 +25,11 @@ def CountSightings(country, specifiedCountry):
             numSightings = numSightings + 1
 
     return numSightings
-#return takes the local varaibles and passes it back to the main program 
-#function count sighting creates an array that serarches through ever occurance where the specified country is named and if it is found the number of occurances for this country will increase 
+
 
 def DisplaySightings(specifiedCountry, numSightings):
     print(specifiedCountry, numSightings)
     pass
-#pass does nothing but lets the program contuine 
-
-#CountYearSightings()
-#Count the sightings each year.
-#IN: thisDate [ ]
-#OUT:
 
 def CountYearSightings(thisDate, specifiedyear):
     yearsightings = 0
